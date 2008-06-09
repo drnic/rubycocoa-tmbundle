13b331ac5e4d7abc5b27af4af199b6cbc09166d6
Dr Nic Williams <drnicwilliams@gmail.com>
2008-06-10 01:15:06 +1000

    fixed the extraneous newline on instance cmd

--- Support/bin/new_ruby_cocoa_instance.rb
+++ Support/bin/new_ruby_cocoa_instance.rb
@@ -4,4 +4,4 @@ require File.dirname(__FILE__) + '/../lib/new_ruby_cocoa_instance'
 current_line  = ENV['TM_CURRENT_LINE']
 column_number = ENV['TM_COLUMN_NUMBER'].to_i
 current_file  = ENV['TM_FILEPATH']
-puts NewRubyCocoaInstance.run(current_line, column_number, current_file)
+print NewRubyCocoaInstance.run(current_line, column_number, current_file)
